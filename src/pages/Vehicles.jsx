b5eac184cf45d2484ac6547e2cf94f97a7d3b225
DavidHz <dave.hzad@gmail.com>
2026-02-20 20:37:10 -0600

    Fix: Adjusted header width

--- src/pages/Vehicles.jsx
+++ src/pages/Vehicles.jsx
@@ -45,7 +45,7 @@ export default function Vehicles() {
             component="main"
             sx={{
                 minHeight: "100vh",
-                width: "100vw",
+                width: "100%",
                 display: "flex",
                 flexDirection: "column"
             }}
